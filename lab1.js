2d485525ff614cc19dc8e4ab0095c648b74e415b
taiprovip3 <taito1doraemon@gmail.com>
2022-08-25 10:14:49 +0700

    lab1

---
+++ lab1.js
@@ -0,0 +1,8 @@
+function cal() {
+    var x = document.createElement("INPUT");
+    x.setAttribute("type", "text");
+    x.setAttribute("value", "Hello World!");
+    document.body.appendChild(x);
+}
+
+console.log("asd");
